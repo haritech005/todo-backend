374c1b0341011e82d9f0198749125306afba46ae
hariharanj-01 <hariharanj@lumel.com>
2025-05-18 17:16:31 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// get the file path from URL of the current module 
+const __filename = fileURLToPath(import.meta.url);
+
+//get the dir name from the file path 
+const __dirname = dirname(__filename);
+// middleware 
+app.use(express.json());
+
+//Serves the HTML from the /public directory 
+// tells express to serve all files from the public folder as static assests/file 
+//Any request for the css files will be resolved to public directory 
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the html file from the /public directory 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+})
