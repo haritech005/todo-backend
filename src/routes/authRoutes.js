03b47e525f20a9f75e500917d2e6dd4bd15b3bc8
haritech005 <j.hariharan005@gmail.com>
2025-05-19 07:38:49 +0530

    password hashed and register values sent to db

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,7 +7,20 @@ const router = express.Router();
 
 // Middleware to check if the user is authenticated /auth/register
 router.post("/register", (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
+
+  //save the username and irreversably encrypted password to the database
+  // encrypt the password
+  const hashedPassword = bcrypt.hashSync(password, 8)
+  //save the new user and hashed password to the database
+try{
+  const insertUser = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+  const result = insertUser.run(username, hashedPassword);
+}
+catch(error){
+  console.error("Error saving user to database:", error);
+  return res.status(500).json({ message: "Internal server error" });
+}
   
 });
 
