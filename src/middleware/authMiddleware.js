d75a1ecb69284bdafa6243feb5d3df91348ddabc
hariharanj-01 <hariharanj@lumel.com>
2025-05-20 20:56:00 +0530

    middleware modified

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,7 +5,11 @@ function authMiddleware(req, res, next) {
     if (!token) {
         return res.status(401).json({ message: "No token Provided" })
     }
-    jwt.verify(token, process.env.JWT_SECRET, () => {
-
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ message: "Invalid Token" })
+        req.userId = decoded.id
+        next()
     })
 }
+
+export default authMiddleware;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 const app = express();
 
@@ -28,7 +29,8 @@ app.get("/", (req, res) => {
 
 //routes
 app.use("/auth", authRoutes);
-app.use("/todos", todoRoutes);
+app.use("/todos", authMiddleware, todoRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
